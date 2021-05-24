6f8b781d4d90fa648a0e0e7d7e56f62ed86873fd
pciftci88 <philipp.ciftci@gmail.com>
2021-05-24 18:30:48 +0200

    Adjusted meta-title and meta-description for Immobilien page.

--- src/pages/immobilien/Immobilien.tsx
+++ src/pages/immobilien/Immobilien.tsx
@@ -11,8 +11,8 @@ export default function Immobilien(props: Props): ReactElement {
     return (
         <Content>
             <Helmet>
-                <title>Immobilien - Finden Sie die passende Immobilie | MyLead24</title>
-                <meta name="description" content="Finden Sie noch heute Ihr Eigenheim." />
+                <title>Immobilienverkauf - Verkaufen Sie Ihre Immobilie zum besten Preis | MyLead24</title>
+                <meta name="description" content="Verkaufen Sie noch heute Ihre Immobilie." />
             </Helmet>
             {props.title && props.title}
             <WidgetInitializer id="tcpp-iframe-iv" scripts='https://form.partner-versicherung.de/widgets/109564/tcpp-iframe-iv/iv-iframe.js'></WidgetInitializer>
