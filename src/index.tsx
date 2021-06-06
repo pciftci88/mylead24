75bcf2ed19bfd57dfd22cd9fa4983453493c83f8
pciftci88 <philipp.ciftci@gmail.com>
2021-06-06 11:03:34 +0200

    Removed unregister for service-worker Added condition to exclude Netlify functin endpoints

--- src/index.tsx
+++ src/index.tsx
@@ -15,10 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// Unregister serviceWorker
-navigator.serviceWorker.getRegistrations().then(function (registrations) {
-  for (let registration of registrations) {
-    registration.unregister()
-  }
-});
